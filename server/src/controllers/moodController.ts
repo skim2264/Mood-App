1d40e8a348fc878373a5bf0ad5021bbba8f0fadd
skim2264 <72099715+skim2264@users.noreply.github.com>
2023-07-04 22:12:06 -0400

    Features to get random recommendations based on mood and adding mood to user profile added

--- server/src/controllers/moodController.ts
+++ server/src/controllers/moodController.ts
@@ -1,9 +1,15 @@
 import { RequestHandler } from "express";
 import MoodModel from "../models/mood";
 import UserMoodModel from "../models/user_mood";
+import AdviceModel from "../models/advice";
+import QuoteModel from "../models/quote";
+import SongModel from "../models/song";
+import UserModel from "../models/user";
 import createHttpError from "http-errors";
 import { assertIsDefined } from "../util/assertIsDefined";
+import { getRandomInt } from "../util/randomNum";
 
+//get list of moods
 export const getAllMoods: RequestHandler = async(req, res, next) => {
   try {
     const moods = await MoodModel.find().exec();
@@ -13,37 +19,63 @@ export const getAllMoods: RequestHandler = async(req, res, next) => {
   }
 };
 
+//get random recommendation based on chosen mood
 export const getRec: RequestHandler = async(req, res, next) => {
+  let rec = {};
+
+  //get random number to help pick random recommendation
+  const randomInt = getRandomInt(3);
+
+  //randomly pick from which list to get recommended list and pick randomly from list
+  if (randomInt == 0) {
+    const adviceList = await AdviceModel.find({mood: req.params.mood}).exec();
+    rec = adviceList[getRandomInt(adviceList.length)];
+  } else if (randomInt == 1) {
+    const quoteList = await QuoteModel.find({mood: req.params.mood}).exec();
+    rec = quoteList[getRandomInt(quoteList.length)];
+  } else if (randomInt == 2) {
+    const songList = await SongModel.find({mood: req.params.mood}).exec();
+    rec = songList[getRandomInt(songList.length)];
+  }
+
   try {
-    res.status(200).json("happy");
+    res.status(200).json(rec);
   } catch (error) {
     next(error);
   }
 };
 
 interface addTodayMood {
-  day?: string,
   mood?: string,
 }
 
+//add mood to user moods list
 export const addMood: RequestHandler<unknown, unknown, addTodayMood, unknown> = async(req, res, next) => {
   const authenticatedUserId = req.session.userId;
 
-  const day = req.body.day;
+  const day = Date.now();
   const mood = req.body.mood;
 
   //get user and append todayMood to moodsList
   try {
     assertIsDefined(authenticatedUserId);
-    
+
     if (!mood || !day) {
       throw createHttpError(400);
     }
-    const todayMood  = await MoodModel.create({
+    
+    //create user_mood in mongoose
+    const todayMood = await UserMoodModel.create({
       day: day,
       mood: mood,
-    })
-    res.status(201).json(todayMood);
+    });
+
+    //get user and add user_mood to moodslist
+    const user = await UserModel.findById(authenticatedUserId).exec();
+    user?.moodsList.push(todayMood._id);
+    await user?.save();
+
+    res.status(201).json(user);
   } catch (error) {
     next(error);
   }
