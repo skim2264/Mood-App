bc36bac718be473b959aba0e5a35158afd4c7565
skim2264 <72099715+skim2264@users.noreply.github.com>
2023-07-29 00:04:12 -0400

    added railway to cors origin

--- server/src/app.ts
+++ server/src/app.ts
@@ -20,7 +20,7 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors({
-  origin: ["https://skim2264.github.io", "http://localhost:3000"],
+  origin: ["https://mood-app-production.up.railway.app", "http://localhost:3000"],
   credentials: true,
 }));
 
