6ca7324c94ac4b20d0792a431d796fcc15fdf233
skim2264 <72099715+skim2264@users.noreply.github.com>
2023-07-28 21:29:59 -0400

    updated cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -20,7 +20,7 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors({
-  origin: "https://skim2264.github.io/Mood-App/",
+  origin: "https://skim2264.github.io/Mood-App",
   credentials: true,
 }));
 
