a7da1b85c84bb7dbbad7656116b85390b6b2e975
skim2264 <72099715+skim2264@users.noreply.github.com>
2023-07-28 21:50:02 -0400

    edited cors origin

--- server/src/app.ts
+++ server/src/app.ts
@@ -20,7 +20,7 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors({
-  origin: "https://skim2264.github.io/",
+  origin: "https://skim2264.github.io",
   credentials: true,
 }));
 
